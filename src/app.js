fe7f0d5f208bce97cee410247f0acf34567814b2
Joanzam <joanzam.com@gmail.com>
2024-06-24 19:14:06 +0000

    joan

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  /**
+   * 1.Generar de cada lista una palabra aleatoria
+   * 2.Concatenar las palabras
+   * 3
+   */
 };
