2313ccc489f43d428a94e62a54bfe9bda4a016df
Joanzam <joanzam.com@gmail.com>
2024-06-24 19:14:32 +0000

    Jona1

--- src/app.js
+++ src/app.js
@@ -16,9 +16,34 @@ window.onload = function() {
     "during my lunch",
     "while I was praying"
   ];
+    function getRandomNumber(lista) {
+let tamanoLista = lista.length
+      let numero = Math.random()
+      let indiceWho = Math.floor(Math.random() * tamanoLista);
+      let excuse = wordWho + " " + wordAction + " " + wordWhat + " " + wordWhen;
+      return excuse;
+    }
+
   /**
    * 1.Generar de cada lista una palabra aleatoria
    * 2.Concatenar las palabras
-   * 3
+   * 3 Mostra en pantalla
    */
+};
+ const generateExcuse = () => {
+    let decimalRandom = Math.random();
+    let indiceWho = Math.floor(decimalRandom * who.length);
+    let indiceAction = Math.floor(decimalRandom * action.length);
+    let indiceWhat = Math.floor(decimalRandom * what.length);
+    let indiceWhen = Math.floor(decimalRandom * when.length);
+    let wordWho = who[indiceWho];
+    let wordAction = action[indiceAction];
+    let wordWhat = what[indiceWhat];
+    let wordWhen = when[indiceWhen];
+
+    let excuse = wordWho + " " + wordAction + " " + wordWhat + " " + wordWhen;
+    return excuse;
+  };
+
+  document.querySelector("#excuse").innerHTML = generateExcuse();
 };
